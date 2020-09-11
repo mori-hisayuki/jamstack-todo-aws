fd28b220403873ed29d6e68cd92def8404563dc9
mori-hisayuki <h.mori0206@gmail.com>
2020-09-11 15:29:52 +0900

    Linkの変更

--- packages/www/gatsby-browser.js
+++ packages/www/gatsby-browser.js
@@ -22,7 +22,7 @@ const authLink = setContext((_, {headers}) => {
 })
 
 const httpLink = new HttpLink({
-    uri: 'https://jamstack-todo-netlify-faunadb.netlify.app/.netlify/functions/graphql',
+    uri: '/.netlify/functions/graphql',
 })
 
 const client = new ApolloClient({
